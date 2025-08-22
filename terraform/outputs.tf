ac0d52c876b87cf07607723d7575b3f6da7572eb
Emmanuel Okoro <40013432+endiesworld@users.noreply.github.com>
2025-08-21 21:15:55 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "app_instance_id" {
   description = "ID of the app EC2 instance"
 }
 
-output "ec2-public_ip" {
+output "ec2_public_ip" {
   value       = aws_instance.app_server.public_ip
   description = "Public IP of the app EC2 instance"
 }
