866ff391e80ed0544db8485b45851232075b5c74
Emmanuel Okoro <40013432+endiesworld@users.noreply.github.com>
2025-08-21 21:33:08 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "app_sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = [ingress.value]
+        cidr_blocks = [var.my_IP, var.jenkins_ip]
         }
     }
 
